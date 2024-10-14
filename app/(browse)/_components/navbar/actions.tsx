080105381ae57c5d26358e5d6cc0f872de6235a7
nhungnguyen-9 <nguyennhung9846@gmail.com>
2024-10-14 22:46:35 +0700

    update prisma model

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -32,6 +32,7 @@ export const Actions = async () => {
                             <span className="hidden lg:block">Dashboard</span>
                         </Link>
                     </Button>
+
                     <UserButton />
                 </div>
             )}
