21b33e2387554419bcfa907e3bf9748ad2f417d4
minhnghia2k3 <brozennvn@gmail.com>
2024-10-16 09:05:43 +0700

    fix: warning at number type

--- lib/stream-service.ts
+++ lib/stream-service.ts
@@ -1,6 +1,6 @@
 import {db} from "@/lib/db";
 
-export const getStreamByUserID = async (userID: number) => {
+export const getStreamByUserID = async (userID: bigint) => {
     return db.stream.findFirst({
         where: {id: userID}
     });
