615034652cb889e219f03fb859f4d16c7da5a8f5
minhnghia2k3 <BrozennVN@gmail.com>
2024-10-16 17:41:14 +0700

    feat: user-viewer-token service

---
+++ hooks/use-viewer-token.ts
@@ -0,0 +1,40 @@
+import { createViewerToken } from "@/actions/token"
+import { useEffect, useState } from "react"
+import { toast } from "sonner"
+import {jwtDecode, JwtPayload} from "jwt-decode"
+
+export const useViewerToken = (hostIdentity: string) => {
+    const [token, setToken] = useState("")
+    const [name, setName] = useState("")
+    const [identity, setIdentity] = useState("")
+
+    useEffect(() => {
+        const createToken = async () => {
+            try{
+                const viewerToken = await createViewerToken(hostIdentity)
+                setToken(viewerToken)
+
+                const decodeToken = jwtDecode(viewerToken) as JwtPayload & {name?: string}
+
+                const name = decodeToken?.name
+                const identity = decodeToken?.jti
+
+                if (identity) {
+                    setIdentity(identity)
+                } 
+
+                if (name) {
+                    setIdentity(name)
+                }
+            }catch{
+                toast.error("Something went wrong!")
+            }
+        }
+
+        createToken()
+    }, [])
+
+    return {
+        token,name,identity
+    }
+}
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -38,3 +38,12 @@ export const getUserByUserName = async (username: string) => {
     return user;
 }
 
+
+export const getUserById = async (userId: string) => {
+    const user = await db.user.findUnique({
+        where: {id: Number(userId)},
+        include: {streams: true}
+    })
+
+    return user
+}
