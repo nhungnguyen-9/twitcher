3846c280c74680c28f0157210e2a99a95bddc23d
minhnghia2k3 <BrozennVN@gmail.com>
2024-10-17 16:11:20 +0700

    Revert "fix: add response at the end of api"
    This reverts commit a5498860097e0a8b18ecca02774c448e13c62db8.

--- app/api/webhooks/livekit/route.ts
+++ app/api/webhooks/livekit/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db";
 import { WebhookReceiver } from "livekit-server-sdk";
 import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 
 const receiver = new WebhookReceiver(
     process.env.LIVEKIT_API_KEY!,
@@ -11,11 +12,13 @@ const receiver = new WebhookReceiver(
 export async function POST(req: Request) {
     const body = await req.text();
   
+    
+
     const headerPayload = headers();
     const authorization = headerPayload.get("Authorization");
   
     if (!authorization) {
-      return new Response("No authorization header", { status: 400 });
+      return new NextResponse("No authorization header", { status: 400 });
     }
   
     const event = await receiver.receive(body, authorization);
@@ -41,6 +44,4 @@ export async function POST(req: Request) {
         }
       })
     }
-
-    return new Response("Success", {status: 200})
   }
